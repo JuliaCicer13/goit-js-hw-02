92cb2b6688e895827e23746bcee2695a4bcf0585
JuliaCicer13 <ciceron131313@gmail.com>
2023-10-29 17:24:21 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -2,26 +2,27 @@
 "use strict"
 
 function getShippingCost(country) {
-    let price;
     switch (country) {
         case `China`:
             return "Shipping to China will cost 100 sredits";
-            break;
+           
         case `Chile`:
+
             return "Shipping to Chile will cost 250 credits";
-            break;
+           
         case `Australia`:
+
             return "Shipping to Australia will cost 170 credits";
-            break;
+            
         case `Jamaica`:
+
             return "Shipping to Jamaica will cost 120 credits";
-            break;
-        
+            
         default: return `Sorry, there is no delivery to your country`;
            
     }
 
-     return `Shipping to ${country}  will cost ${price}  credits`;
+     
 }
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
